87b5d4cb576c42ae8878909a9e4eaf29cc8da6e9
Shine <clanclanov803@gmail.com>
2026-07-30 20:23:52 +0300

    Update cnetsh.sh

--- installer/cnetsh.sh
+++ installer/cnetsh.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-N_DIR=$(find / -path */CNET/installer/ 2>/dev/null)
-echo "CNET-install-script -> started ($N_DIR/cnetsh.sh)"
-cd "$N_DIR"
-cd ..
-cd ./src
-gcc -shared -fPIC -O2 -DNDEBUG cnet.c -o /usr/local/lib/cnet.so
-echo "Compiled cnet.c -> cnet.so and moved into /usr/local/lib/"
-cd ..
-cd ./headers
-mv ./* /usr/local/include/
-LD_PATHBIN=$(find / -name ldconfig 2>/dev/null)
-LD_CONFBIN="/etc/ld.so.conf.d/cnet.conf"
+GCCH="gcc"
+if [ -f "$GCCH" ]; then
+    N_DIR=$(find / -path */CNET/installer/ 2>/dev/null)
+    echo "CNET-install-script -> started ($N_DIR/cnetsh.sh)"
+    cd "$N_DIR"
+    cd ..
+    cd ./src
+    gcc -shared -fPIC -O2 -DNDEBUG cnet.c -o /usr/local/lib/cnet.so
+    echo "Compiled cnet.c -> cnet.so and moved into /usr/local/lib/"
+    cd ..
+    cd ./headers
+    mv ./* /usr/local/include/
+    LD_PATHBIN=$(find / -name ldconfig 2>/dev/null)
+    LD_CONFBIN="/etc/ld.so.conf.d/cnet.conf"
 
-if [ -f "$LD_CONFBIN" ]; then
-    echo "ldconfig-conf -> found ($N_DIR/cnetsh.sh)"
-else
-    echo "ldconfig-conf -> not found -> start create ($N_DIR/cnetsh.sh)"
-    echo "/usr/local/lib" | tee "$LD_CONFBIN" 2>/dev/null
-fi
+    if [ -f "$LD_CONFBIN" ]; then
+        echo "ldconfig-conf -> found ($N_DIR/cnetsh.sh)"
+    else
+        echo "ldconfig-conf -> not found -> start create ($N_DIR/cnetsh.sh)"
+        echo "/usr/local/lib" | tee "$LD_CONFBIN" 2>/dev/null
+    fi
 
-if [ -f "$LD_PATHBIN" ]; then
-    echo "ldconfig -> successful ($N_DIR/cnetsh.sh)"
-    "$LD_PATHBIN"
-else
-    echo "ldconfig -> error ($N_DIR/cnetsh.sh)"
+    if [ -f "$LD_PATHBIN" ]; then
+        echo "ldconfig -> successful ($N_DIR/cnetsh.sh)"
+        "$LD_PATHBIN"
+    else
+        echo "ldconfig -> error ($N_DIR/cnetsh.sh)"
+    fi
 fi
