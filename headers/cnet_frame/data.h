9698b0a503e6b621c90e71d90e514a774c565687
Shine <clanclanov803@gmail.com>
2026-08-18 16:43:49 +0300

    Add FCS field to various cnet_frame structs

--- headers/cnet_frame/data.h
+++ headers/cnet_frame/data.h
@@ -8,23 +8,27 @@
 struct cnet_frame_data
 {
 	struct cnet_frame_hdr cnet_hdr;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_null
 {
 	struct cnet_frame_hdr cnet_hdr;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_data_wds
 {
 	struct cnet_frame_hdr cnet_hdr;
 	uint8_t addr4[6];
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_qos_data
 {
 	struct cnet_frame_hdr cnet_hdr;
 	uint16_t qos;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_qos_data_wds
@@ -32,6 +36,7 @@ struct cnet_frame_qos_data_wds
 	struct cnet_frame_hdr cnet_hdr;
 	uint8_t addr4[6];
 	uint16_t qos;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_qos_data_ht
@@ -39,6 +44,7 @@ struct cnet_frame_qos_data_ht
 	struct cnet_frame_hdr cnet_hdr;
 	uint16_t qos;
 	uint32_t ht;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_qos_data_merge
@@ -47,12 +53,14 @@ struct cnet_frame_qos_data_merge
 	uint8_t addr4[6];
 	uint16_t qos;
 	uint32_t ht;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 struct cnet_frame_qos_null
 {
 	struct cnet_frame_hdr cnet_hdr;
 	uint16_t qos;
+    uint8_t fcs[4];
 }__attribute__((packed));
 
 #endif
